a3ba4a717224ac5d3263389c443382d62c8276f9
Eugene Miroshnichenko <jenainours@yandex.ru>
2022-12-02 22:35:37 +0300

    add menu item for sd_staff module to website menu

---
+++ app/addons/sd_staff/schemas/menu/menu.post.php
@@ -0,0 +1,11 @@
+<?php
+
+$schema['central']['website']['items']['sd_staff'] = array(
+    'attrs' => array(
+        'class'=>'is-addon'
+    ),
+    'href' => 'sd_staff.manage',
+    'position' => 500
+);
+
+return $schema;
